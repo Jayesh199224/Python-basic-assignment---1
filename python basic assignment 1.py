d3a924f71c8bc17c49783c061da2845e098a32ab
Jayesh199224 <79449164+Jayesh199224@users.noreply.github.com>
2021-03-15 00:52:59 +0530

    Add files via upload

---
+++ python basic assignment 1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1
+
+
+# In[4]:
+
+
+#7. What should the values of the following two terms be?
+
+'spam' + 'spamspam'
+
+
+# In[5]:
+
+
+'spam' * 3
+
+
+# In[34]:
+
+
+#8. Why is it that eggs is a true variable name but 100 is not?
+eggs = "eggs"
+
+
+# In[35]:
+
+
+eggs
+
+
+# In[36]:
+
+
+100 = 100
+
+
+# In[38]:
+
+
+#10. What is the error caused by this expression? What would you do about it?
+
+'I have eaten ' + 99 + ' burritos.'
+
+
+# In[ ]:
+
+
+
+
